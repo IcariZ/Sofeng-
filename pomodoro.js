e9b850ce9588b3babb7ee9e1df5632792862dca3
IcariZ <89731969+IcariZ@users.noreply.github.com>
2024-05-06 23:15:03 +0700

    Add files via upload

--- pomodoro.js
+++ pomodoro.js
@@ -1,10 +1,50 @@
-function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
+var timer;
+var ele = document.getElementById('jam');
+console.log("SCRIPT MASUK");
+var durasi = 0;
+
+(function(){
+    if (!durasi)
+    {
+        alert("Durasi kosong")
+    }
+    else
+    {
+        // timer = setInterval(()=>{
+        //     ele.innerHTML = '00:'+sec;
+        //     sec--;
+        // },1000)
+    }
+
+})()
+
+
+function tambah()
+{
+    durasi = durasi + 300; //5 menit
+    
+    var detik   = 0;
+    var menit   = 0;
+    var jam     = 0;
+
+    if (durasi > 60)
+    {
+        detik = durasi%60;
+        menit = Math.floor(durasi/60);
+        if (menit > 60)
+        {
+            jam++;
+            menit =0;
         }
-    }, 1000);
+    }
+    alert("Durasi : "+jam+":"+menit+":"+detik);
+}
+function pause()
+{
+    clearInterval(timer);
+}
+
+function start()
+{
+    
 }
