a27695551fdfa15925dec298748162d6ad4f58be
crmell <jessicaseren26@gmail.com>
2024-06-05 10:07:21 +0700

    fix log in validation

--- script.js
+++ script.js
@@ -20,18 +20,25 @@ iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
 
-// console.log("dor")
+// localStorage.clear();
+
+// for(var i=0, len=localStorage.length; i<len; i++) {
+//     var key = localStorage.key(i);
+//     var value = localStorage[key];
+//     console.log(key + " => " + value);
+// }
 
 function signup(e){
     event.preventDefault();
 
     var email = document.getElementById('email').value;
-    var username = document.getElementById('username').value;
+    console.log(email)
+
     var pass = document.getElementById('reg_password').value;
+    console.log(pass)
     
     var user = {
         email: email,
-        username: username,
         password: pass,
     };
 
@@ -45,19 +52,19 @@ function loginFunc(e){
     console.log("in");
     event.preventDefault();
 
-    var username = document.getElementById('username').value;
-    console.log(username)
+    var email = document.getElementById('email').value;
+    console.log(email)
     var pass = document.getElementById('password').value;
     console.log(pass)
     var result = document.getElementById('result');
 
-    var user = localStorage.getItem(username);
+    var user = localStorage.getItem(email);
     var data = JSON.parse(user);
     console.log(data);
 
     if (user == null){
         result.innerHTML = 'Oops! Invalid crendetials! Try again.';
-    } else if (username == data.username && pass == data.password){
+    } else if (email == data.email && pass == data.password){
         result.innerHTML = 'logged in!';
     } else {
         result.innerHTML = 'Oops! Invalid crendetials! Try again.';
