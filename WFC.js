ab1e47e994a06bf8238c255d48ba5f8725929b1c
IcariZ <89731969+IcariZ@users.noreply.github.com>
2024-06-12 20:58:36 +0700

    Update WFC.js

--- WFC.js
+++ WFC.js
@@ -1,9 +1,11 @@
-const listCafe = document.querySelector('.place-card');
+const jakut = document.querySelector('.jakut');
+const jaksel = document.querySelector('.jaksel');
 const frame = document.querySelector('.places');
 const option = document.querySelector('.list-box1');
-const closing = document.querySelector('x-butt');
+const closing = document.querySelector('.x-butt');
+const option2 = document.querySelector('.list-box2');
 
-listCafe.addEventListener("click", ()=>
+jakut.addEventListener("click", ()=>
 {
     frame.style.display = "none";
     option.style.display = "block";
@@ -13,4 +15,10 @@ closing.addEventListener("click", ()=>
 {
     frame.style.display = "block";
     option.style.display ="none";
+});
+
+jaksel.addEventListener("click", ()=>
+{
+    frame.style.display = "none";
+    option.style.display = "block";
 });
